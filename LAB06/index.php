37b388feea59f776d21d711ee09206691d0e44b6
1909248 <stefanoschatz13@gmail.com>
2020-02-14 17:54:39 +0000

    lab6 my first php

--- LAB06/index.php
+++ LAB06/index.php
@@ -1,15 +1,32 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-
 </head>
 <body>
 <p>
     <?php
-    echo "Hello World"
+    echo "Hello World";
+    echo "Hello,","","world","!";
+    echo 5*7;
+    $myname ="Frodo Baggins";
+    $myage=111;
+    echo"My name is ",$myname,"and i am",$myage
+    ?>
+    <?php
+    echo "i get printed!";
+//    i dont i am a comment
+    ?>
+<!--second question-->
+    <?php
+    $name ="Edgar";
+    if($name=="Simon"){
+        print"I know you";
+    }
+    else{
+        print"Who are you?";
+    }
     ?>
 </p>
 </body>
-
 </html>
 
